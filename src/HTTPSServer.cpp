27e13f815b2566e3e48a162643396f01bac145cc
KaloNK <GitHub@TheKNK.Net>
2021-11-29 17:12:23 +0200

    Some fixes to setupSSLCTX()

--- src/HTTPSServer.cpp
+++ src/HTTPSServer.cpp
@@ -64,10 +64,10 @@ int HTTPSServer::createConnection(int idx) {
  * This method configures the ssl context that is used for the server
  */
 uint8_t HTTPSServer::setupSSLCTX() {
-  _sslctx = SSL_CTX_new(TLSv1_2_server_method());
+  _sslctx = SSL_CTX_new(TLS_server_method());
   if (_sslctx) {
-    // Set SSL Timeout to 5 minutes
-    SSL_CTX_set_timeout(_sslctx, 300);
+    // Set SSL Timeout to 60 seconds
+    SSL_CTX_set_timeout(_sslctx, 60);
     return 1;
   } else {
     _sslctx = NULL;
